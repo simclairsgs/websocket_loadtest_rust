6b149e8b5f7d544ed001bd6c43b6555f9be22350
george.sam <george.sam@zohocorp.com>
2024-12-18 16:16:59 +0530

    load test changes

--- start_load_test.sh
+++ start_load_test.sh
@@ -17,9 +17,9 @@ cp target/release/mgserver_loadtest_impl ./loadtest.bin
 
 for remote in "${test_ips[@]}";
 do
-	echo "START IN $remote"
+	echo "SYNC IN $remote"
 	sshpass -P "password" -p "$spwd" ssh sas@$remote "mkdir -p ~/george/loadtest/websocket_loadtest_rust" && sshpass -P "password" -p "$spwd" scp loadtest.bin start_load_slave.sh stop_load_slave.sh sas@$remote:~/george/loadtest/websocket_loadtest_rust/
-	sshpass -P "password" -p "$spwd" ssh sas@$remote "sh ~/george/loadtest/websocket_loadtest_rust/start_load_slave.sh"
+	# sshpass -P "password" -p "$spwd" ssh sas@$remote "sh ~/george/loadtest/websocket_loadtest_rust/start_load_slave.sh"
 done
 
 #sh ~/george/loadtest/websocket_loadtest_rust/start_load_slave.sh
